3ed2ebfd6f0293c0563e504fefdf9c5b657f2c31
laurian staicu <staicu4@gmail.com>
2017-04-12 13:38:40 -0400

    added functionality to the interface

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,15 +8,14 @@ namespace MediaManager
     public partial class App : Application
     {
 
-        void AppStartup(object sender, StartupEventArgs args)
+        void StartApp(object sender, StartupEventArgs args)
         {
             WindowPhotoSelect mainWindow = new WindowPhotoSelect();
             mainWindow.Show();
 
             mainWindow.Photos = (MediaList)(this.Resources["Photos"] as ObjectDataProvider).Data;
             mainWindow.Photos.Path = "..\\..\\photos";
-
-            
+           
             
         }
 

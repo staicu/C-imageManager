4625ffa2c2993ca2aa488e2a8e55d32075373507
Bert Joseph-Paré <biterrormusic@hotmail.com>
2017-04-18 11:24:06 -0400

    changed adorner

--- adorner.cs
+++ adorner.cs
@@ -1,5 +1,6 @@
 #define VISUALCHILD
 
+
 using System;
 using System.IO;
 using System.Windows;
@@ -33,7 +34,7 @@ namespace MediaManager
             _rubberband = new System.Windows.Shapes.Path();
             _rubberband.Data = _geometry;
             _rubberband.StrokeThickness = 2;
-            _rubberband.Stroke = System.Windows.Media.Brushes.Yellow;
+            _rubberband.Stroke = System.Windows.Media.Brushes.OrangeRed;
             _rubberband.Opacity = .6;
             _rubberband.Visibility = Visibility.Hidden;
             AddVisualChild(_rubberband);
@@ -95,88 +96,5 @@ namespace MediaManager
     }
 #endif
 
-#if NoVISUALCHILD
-
-
-    public class RubberbandAdorner : Adorner
-    {
-
-        private UIElement _adornedElement;
-        private bool _showRect;
-        private Window1 _window;
-        SolidColorBrush _brush;
-        Pen _pen;
-        private Rect _selectRect;
-        public Rect SelectRect { get { return _selectRect; } set { _selectRect = value; } }
-        public bool ShowRect { get { return _showRect; } set { _showRect = value; } }
-        public Window1 Window { set { _window = value; } }
-
-        public RubberbandAdorner(UIElement adornedElement)
-            : base(adornedElement)
-        {
-            _adornedElement = adornedElement;
-            _selectRect = new Rect();
-            _brush = new SolidColorBrush();
-            _brush.Color = Colors.Yellow;
-            _brush.Opacity = .6;
-            _pen = new Pen();
-            _pen.Thickness = 2;
-            _pen.Brush = _brush;
-            _showRect = false;
-            MouseMove += new MouseEventHandler(DrawSelection);
-            MouseUp += new MouseButtonEventHandler(EndSelection);
-        }
-
-        public void StartSelection(Point anchorPoint)
-        {
-            _anchorPoint = anchorPoint;
-            _selectRect.Size = new Size(0, 0);
-            _selectRect.Location = _anchorPoint;
-            if (!_showRect)
-                _showRect = true;
-        }
-
-        private void DrawSelection(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                Point mousePosition = e.GetPosition(_adornedElement);
-                if (mousePosition.X < _anchorPoint.X)
-                    _selectRect.X = mousePosition.X;
-                else
-                    _selectRect.X = _anchorPoint.X;
-                if (mousePosition.Y < _anchorPoint.Y)
-                    _selectRect.Y = mousePosition.Y;
-                else
-                    _selectRect.Y = _anchorPoint.Y;
-                _selectRect.Width = Math.Abs(mousePosition.X - _anchorPoint.X);
-                _selectRect.Height = Math.Abs(mousePosition.Y - _anchorPoint.Y);
-                InvalidateArrange();
-                AdornerLayer layer = AdornerLayer.GetAdornerLayer(_adornedElement);
-                layer.InvalidateArrange();
-            }
-        }
-
-        private void EndSelection(object sender, MouseButtonEventArgs e)
-        {
-            if (3 >= _selectRect.Width || 3 >= _selectRect.Height)
-                ShowRect = false;
-            else
-                _window.CropButton.IsEnabled = true;
-            ReleaseMouseCapture();
-        }
-
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            if (_showRect)
-            {
-                base.OnRender(drawingContext);
-                drawingContext.DrawRectangle(Brushes.Transparent, _pen, _selectRect);
-            }
-            else
-                return;
-        }
-    }
-#endif
-
 }
+ 
